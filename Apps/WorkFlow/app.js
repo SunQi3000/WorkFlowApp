61816779dc2c68cab9b93ae394991134f675b3d4
sunqi3000 <sunq3000@163.com>
2017-11-25 15:57:55 +0800

    app读取config.json
    Change-Id: Ia6491421820b29eb3ed9eb3828758c1b590811b8
    Signed-off-by: sunqi3000 <sunq3000@163.com>

--- Apps/WorkFlow/app.js
+++ Apps/WorkFlow/app.js
@@ -1,48 +1,64 @@
 define(["utils"], function(utils) {
-    return {
-        init: function(PM) {
-            PM.start({            	
-                host: "http://10.23.71.57/workflowwebservice55/query.asmx",
-                helppdf: "http://10.23.71.57/workflowwebservice55/pdfjs/web/viewer.html?file=V60WorkFlow-help.pdf",                
-                root: "index",
-                //全局变量
+	return {
+		init: function(PM) {
+			var hostUrl = "";
+			var helppdf = "";
+			var SwitchCouldLogin = "false";
+			//同步
+			$.ajaxSettings.async = false;
+			$.getJSON("/config.json", function(data) {
+				$.each(data, function(index, obj) {
+					if(index == "host") {
+						hostUrl = obj.toString() + "/query.asmx";
+						helppdf = obj.toString() + "/pdfjs/web/viewer.html?file=V60WorkFlow-help.pdf";
+					}
+					if(index == "yyCouldLogin") {
+						SwitchCouldLogin = obj;
+					}
+				})
+			});
+			//关闭同步
+			$.ajaxSettings.async = true;
+
+			PM.start({
+				host: hostUrl,
+				helppdf: helppdf,
+				root: "index",
+				//全局变量
 				yyUserCode: "",
-				yyCouldLogin:true,
-                customerComponents: {
+				yyCouldLogin: SwitchCouldLogin,
+				customerComponents: {},
+				baseSize: {
+					width: 375,
+					height: 667
+				},
+				beforeSendAjax: function(config) {
+					//发送ajax请求的时候全局捕获函数,前提是使用页面示例对象的ajax方法
+					//这样可以减少ajax参数工作量
+					//var pageInstance = config.pageviewInstance;
+					//将当前页面的url参数token赋值给ajax请求参数
+					//config.data.token = pageInstance.params["token"];
+				},
+				beforeGo: function(config) {
+					//跳转页面的时候 可以在这里进行捕获 可以添加处理跳转的参数
+					//这样可以减少跳转传参数工作量
+					//var pageInstance = config.pageviewInstance;
+					//将当前页面的url参数token带给下一页
+					//config.params.token = pageInstance.params.token;
+				},
+				onAjaxResponse: function(params) {
+					//ajax的响应的全局捕获机制
+					/*if(!params.isSuccess){
+						alert(params.data.msg);
+					}*/
 
-                },
-                
-                baseSize: {
-                    width: 375,
-                    height: 667
-                },
-                beforeSendAjax: function(config) {
-                	 //发送ajax请求的时候全局捕获函数,前提是使用页面示例对象的ajax方法
-                    //这样可以减少ajax参数工作量
-                    //var pageInstance = config.pageviewInstance;
-                    //将当前页面的url参数token赋值给ajax请求参数
-                    //config.data.token = pageInstance.params["token"];
-                },
-                beforeGo: function(config) {
-                	//跳转页面的时候 可以在这里进行捕获 可以添加处理跳转的参数
-                    //这样可以减少跳转传参数工作量
-                    //var pageInstance = config.pageviewInstance;
-                    //将当前页面的url参数token带给下一页
-                    //config.params.token = pageInstance.params.token;
-                },
-                onAjaxResponse: function(params) {
-                	//ajax的响应的全局捕获机制
-                	/*if(!params.isSuccess){
-                		alert(params.data.msg);
-                	}*/
-                	
-                },
-                routerChange: function(arg) {
-                	//页面路由的全局捕获机制
-                    //return false 禁止页面跳转
-                }
-            });
+				},
+				routerChange: function(arg) {
+					//页面路由的全局捕获机制
+					//return false 禁止页面跳转
+				}
+			});
 
-        }
-    };
+		}
+	};
 });
--- Apps/WorkFlow/server.js
+++ Apps/WorkFlow/server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname));
 
-app.listen(1212, function() {
+app.listen(3333, function() {	
     console.log("====================");
-	console.log('http://localhost:1212/dist/index.html, Ctrl+C to stop');
+	console.log('http://localhost:3333/dist/index.html, Ctrl+C to stop');
+	console.log("注意：移动审批服务已启动，关闭该窗口服务将停止.");
     console.log("====================");
 })
